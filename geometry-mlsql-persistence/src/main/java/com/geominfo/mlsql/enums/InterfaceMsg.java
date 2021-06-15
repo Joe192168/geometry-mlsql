c3318168732656cf06194e697584e2da30be7ddf
刘飞 <lfei@geominfo.com>
2021-06-15 14:12:49 +0800

    新增关于Mlsql接口返回信息和系统自定义接口返回信息

--- geometry-mlsql-persistence/src/main/java/com/geominfo/mlsql/enums/InterfaceMsg.java
+++ geometry-mlsql-persistence/src/main/java/com/geominfo/mlsql/enums/InterfaceMsg.java
@@ -18,7 +18,18 @@ public enum InterfaceMsg {
     UPDATE_ERROR("修改时发生异常！"),
     UPDATE_SUCCESS("修改成功！"),
     DELETE_ERROR("删除时发生异常！"),
-    DELETE_SUCCESS("删除成功！"),;
+    DELETE_SUCCESS("删除成功！"),
+    INSERT_SPACE_MEMBER_FAIL("新增工作空间成员失败！"),
+    INSERT_SPACE_MEMBER_SUCCESS("新增工作空间成员成功！"),
+    PARAM_ERROR("参数错误！"),
+    SET_DEFAULT_SPACE_SUCCESS("设置默认工作空间成功！"),
+    SET_DEFAULT_SPACE_FAIL("设置默认工作空间失败！"),
+    SET_DEFAULT_SPACE_ERROR("设置默认工作空间时发生异常！"),
+    SPACE_NOT_TRANSFER("默认空间不能转让！"),
+    TRANSFER_SPACE_SUCCESS("转让工作空间成功！"),
+    TRANSFER_SPACE_FAIL("转让工作空间失败！"),
+    AUTH_ERROR("调用身份服务平台出现异常，请稍后再试！"),
+    ;
 
     private String msg;
 
