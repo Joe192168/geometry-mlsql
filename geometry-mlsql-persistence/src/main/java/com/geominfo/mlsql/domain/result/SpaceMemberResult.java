a4760ff2c675d75ab476ded3711f74f6f056684f
刘飞 <lfei@geominfo.com>
2021-06-23 16:19:06 +0800

    新增工作空间新增参数实体 新增工作空间成员参数实体 新增工作空间和空间成员展示实体 增加接口和接口参数注解

---
+++ geometry-mlsql-persistence/src/main/java/com/geominfo/mlsql/domain/result/SpaceMemberResult.java
@@ -0,0 +1,11 @@
+package com.geominfo.mlsql.domain.result;
+
+/**
+ * @program: geometry-bi
+ * @description:
+ * @author: LF
+ * @create: 2021/6/23 14:47
+ * @version: 1.0.0
+ */
+public class SpaceMemberResult {
+}
---
+++ geometry-mlsql-persistence/src/main/java/com/geominfo/mlsql/domain/result/WorkSpaceInfoResult.java
@@ -0,0 +1,11 @@
+package com.geominfo.mlsql.domain.result;
+
+/**
+ * @program: geometry-bi
+ * @description:
+ * @author: LF
+ * @create: 2021/6/23 11:15
+ * @version: 1.0.0
+ */
+public class WorkSpaceInfoResult {
+}
